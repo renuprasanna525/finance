8f8721175ae2f1e3f2eb1e090f9d5a87f616d0dc
renusana525@gmail.com <renusana525@gmail.com>
2026-09-23 18:10:57 +0530

    Configure Django production settings

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get(
     "SECRET_KEY", "django-insecure-simple-demo-key-change-in-production"
 )
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
